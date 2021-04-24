71b41a86e7150ae03e77976229b1a0053e572602
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-24 11:00:56 -0300

    controle pelo slider e ajeitando o shuffle

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -23,6 +23,7 @@ type PlayerContextData = {
   toggleShuffle: () => void;
   playNext: () => void;
   playPrevious: () => void;
+  clearPlayerState: () => void;
   hasNext: boolean;
   hasPrevious: boolean;
 };
@@ -70,8 +71,13 @@ export function PlayerContextProvider({
     setIsPlaying(state);
   }
 
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
   const hasPrevious = currentEpisodeIndex > 0;
-  const hasNext = (currentEpisodeIndex + 1) < episodeList.length
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length
 
   function playNext() {
     if (isShuffling) {
@@ -106,6 +112,7 @@ export function PlayerContextProvider({
         setPlayingState,
         hasNext,
         hasPrevious,
+        clearPlayerState,
       }}
     >
       {children}
